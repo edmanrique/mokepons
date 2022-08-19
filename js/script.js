7205757a2178a7066ff1fb3afa87a7d24dd8bcb3
edmanriquer_ <emanrique80713@ufide.ac.cr>
2022-08-19 03:08:18 -0600

    Attacks from player to npc

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-function startGame(){
+function startGame() {
     let btPet = document.getElementById('bt-pet');
     btPet.addEventListener('click', selectPet);
 }
 
-function randomPet(min, max){
+function randomPet(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function selectPet(){
+function selectPet() {
     let pet = document.getElementById('pet');
     let selected = pet.options[pet.selectedIndex].text;
 
@@ -20,11 +20,11 @@ function selectPet(){
     selectEnemyPet(selected);
 }
 
-function selectEnemyPet(player){
+function selectEnemyPet(player) {
     let rPet = randomPet(1, 8);
     let enemyPet = ''
 
-    switch(rPet){
+    switch (rPet) {
         case 1:
             enemyPet = 'Hipodoge 💧';
             break;
@@ -51,7 +51,7 @@ function selectEnemyPet(player){
             break;
     }
 
-    if(enemyPet == player){
+    if (enemyPet == player) {
         return selectEnemyPet(player);
     }
 
@@ -60,6 +60,73 @@ function selectEnemyPet(player){
 
     let npcHealth = document.getElementById('enemy-pet-health');
     npcHealth.innerHTML = '❤️❤️❤️';
+
+    selectMovement(player);
+}
+
+function selectMovement(player) {
+    let firstElement = player.substr(player.length - 1, 1);
+    let secondElement = player.charAt(player.length - 3, 1);
+    let hybrid = false;
+
+    if (secondElement == '\uDD25' || secondElement == '\uDCA7' || secondElement == '\uDF31') {
+        console.log('Hybrid')
+        hybrid = true;
+    }
+
+    if (hybrid) {
+        if (firstElement == '\uDD25') {
+            setMovements('Forest fire', 'Flaming rock', 'Chrysalis');
+        } else if (firstElement == '\uDCA7') {
+            setMovements('Earthly rain', 'Aquake', 'Mud Bath');
+        }
+    } else {
+
+        if (firstElement == '\uDD25') {
+            setMovements('Burning flames', 'Wallfire', 'Pyrohealth')
+        } else if (firstElement == '\uDCA7') {
+            setMovements('Tsunami', 'Frosty', 'Relax waves')
+        } else {
+            setMovements('Earthquake', 'Eternal mountains', 'Magic herbs')
+        }
+    }
+}
+
+function setMovements(attack, defense, healing) {
+    let btMovement1 = document.getElementById('bt-movement1');
+    let btMovement2 = document.getElementById('bt-movement2');
+    let btMovement3 = document.getElementById('bt-movement3');
+
+    btMovement1.innerHTML = attack;
+    btMovement2.innerHTML = defense;
+    btMovement3.innerHTML = healing;
+
+    figth(btMovement1, btMovement2, btMovement3);
+}
+
+function figth(btnAttack, btnDefense, btnHealing) {
+    btnAttack.addEventListener('click', attack);
+    btnDefense.addEventListener('click', defense);
+    btnHealing.addEventListener('click', healing);
+}
+
+function attack() {
+    playerName()
+    document.getElementById('movement-player').innerHTML = document.getElementById('bt-movement1').innerHTML;
+}
+
+function defense() {
+    playerName()
+    document.getElementById('movement-player').innerHTML = document.getElementById('bt-movement2').innerHTML;
+}
+
+function healing() {
+    playerName()
+    document.getElementById('movement-player').innerHTML = document.getElementById('bt-movement3').innerHTML;
+}
+
+function playerName(){
+    document.getElementById('my-pet-movement').innerHTML = document.getElementById('my-pet').innerHTML
 }
 
 window.addEventListener('load', startGame);
