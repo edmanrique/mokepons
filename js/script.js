bfe22944019c36d4138688829fb349ffc05e148e
edmanriquer_ <emanrique80713@ufide.ac.cr>
2022-08-18 18:52:35 -0600

    Dynamic HTML to enemy's pet

--- js/script.js
+++ js/script.js
@@ -3,15 +3,63 @@ function startGame(){
     btPet.addEventListener('click', selectPet);
 }
 
-function selectPet(){
-    pet = document.getElementById('pet');
-    selected = pet.options[pet.selectedIndex].text;
+function randomPet(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-    player = document.getElementById('my-pet')
+function selectPet(){
+    let pet = document.getElementById('pet');
+    let selected = pet.options[pet.selectedIndex].text;
+
+    let player = document.getElementById('my-pet')
     player.innerHTML = selected;
 
-    playerHealth = document.getElementById('my-pet-health');
+    let playerHealth = document.getElementById('my-pet-health');
     playerHealth.innerHTML = '❤️❤️❤️';
+
+    selectEnemyPet(selected);
+}
+
+function selectEnemyPet(player){
+    let rPet = randomPet(1, 8);
+    let enemyPet = ''
+
+    switch(rPet){
+        case 1:
+            enemyPet = 'Hipodoge 💧';
+            break;
+        case 2:
+            enemyPet = 'Suggles 💧';
+            break;
+        case 3:
+            enemyPet = 'Capipepo 🌱';
+            break;
+        case 4:
+            enemyPet = 'Todd 🌱';
+            break;
+        case 5:
+            enemyPet = 'Ratigueya 🔥';
+            break;
+        case 6:
+            enemyPet = 'Kailamor 🔥';
+            break;
+        case 7:
+            enemyPet = 'Tucapalma 💧🌱';
+            break;
+        case 8:
+            enemyPet = 'Pydos 🌱🔥';
+            break;
+    }
+
+    if(enemyPet == player){
+        return selectEnemyPet(player);
+    }
+
+    let npc = document.getElementById('enemy-pet');
+    npc.innerHTML = enemyPet;
+
+    let npcHealth = document.getElementById('enemy-pet-health');
+    npcHealth.innerHTML = '❤️❤️❤️';
 }
 
 window.addEventListener('load', startGame);
